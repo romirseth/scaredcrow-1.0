cf61f66e81a3991ce17ee4a88aa146c04772a221
romirseth <romirseth11@gmail.com>
2025-09-09 22:23:32 -0400

    progress bar

--- src/Live/Live.js
+++ src/Live/Live.js
@@ -3,7 +3,7 @@ import './Live.css';
 import '../index.css';
 import scaredcrow from '../_images/Scaredcrow.png';
 import candledivider from '../_images/candledivider.gif';
-import understanding from '../_images/Understanding.JPG';
+import understanding from '../_images/spencer and aiden.JPG';
 import guitar from '../_images/Guitar.JPG';
 import matt from '../_images/Matt.JPG';
 
@@ -31,6 +31,9 @@ function Live() {
             </div>
             {/* <img src={candledivider} style={{transform: 'rotate(180deg)', justifySelf: 'center', width: '300px'}} alt="logo"/> */}
 
+            {/* <div className="progress-bar"></div> */}
+            <div className="play-time"></div>
+
             <div className='player-controls'>
                 <div className="next-prev">
                   <div className="solid-prev"></div>
@@ -44,7 +47,7 @@ function Live() {
             </div>
             
             {/* <img src={candledivider} style={{transform: 'rotate(180deg)', justifySelf: 'center', width: '300px'}} alt="logo"/> */}
-            <div style={{fontFamily: 'PixTimes', color: 'rgba(171, 170, 255, 1)', alignContent: 'center', fontSize: '20.5px', width: '60%'}}>
+            <div style={{fontFamily: 'PixTimes', color: 'rgba(171, 170, 255, 1)', alignContent: 'center', fontSize: '16px', width: '60%'}}>
               <a href="mailto:scaredcrowband@gmail.com" className='lv-contact-us'>scaredcrowband@gmail com</a>
             </div>
           </div>
