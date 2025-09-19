a5a36743c803831715a1848d213a9d4c2fb7969c
romirseth <romirseth11@gmail.com>
2025-09-18 22:02:30 -0400

    corp / wicb

--- src/Originals/OriginalTracks.js
+++ src/Originals/OriginalTracks.js
@@ -1,14 +1,15 @@
 // audio files
 import dutchy from '../_audio/Flying-Dutchman.mp3';
-import wicb from '../_audio/When I Come Back (AW) - 09Sep25.mp3'
-import corp from '../_audio/Corp Throwaway - 10Sep25.mp3';
+import corp from '../_audio/Corp Throwaway Final (First Vocal Pass) - 07Aug25.mp3';
+import wicb from '../_audio/When I Come Back (AW Drums Edits and Other Tweaks) - 14Sep25.mp3'
 import fringe from '../_audio/On the Fringe (Build Back) - 30Apr25.mp3';
 
 // audio thumbnails
 import dutchyTN from '../_thumbnails/Dutchman_Cropped.webp';
+import corpTN from '../_thumbnails/Aiden flow.webp';
 import wicbTN from '../_thumbnails/Matt hehe.webp';
 import fringeTN from '../_thumbnails/Romir dope pt 2.webp';
-import corpTN from '../_thumbnails/Aiden flow.webp';
+
 
 export const tracks = [
     {
@@ -19,13 +20,13 @@ export const tracks = [
     },
     {
         id: 1,
-        title: 'Corporate Throwaway (Demo)',
+        title: 'Corporate Throwaway',
         src: corp,
         thumbnail: corpTN,
     },
     {
         id: 2,
-        title: 'When I Come Back (Demo)',
+        title: 'When I Come Back',
         src: wicb,
         thumbnail: wicbTN,
     },
